92e4202194bdf6df9be314fd1fd43082072f625a
cdvries88 <cdvries88@gmail.com>
2026-04-21 20:41:09 +0200

    Add Android.mk configuration for HeadwindMDM

--- HeadwindMDM/Android.mk
+++ HeadwindMDM/Android.mk
@@ -1,1 +1,30 @@
+LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-com.hmdm.launcher.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := default-permissions-com.hmdm.launcher.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/default-permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := HeadwindMDM
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := hmdm-6.34-master.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_REQUIRED_MODULES := privapp-permissions-com.hmdm.launcher.xml default-permissions-com.hmdm.launcher.xml
+LOCAL_PRODUCT_MODULE := true
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+endif
+
+include $(BUILD_PREBUILT)
